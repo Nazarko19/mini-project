128368476ee731dd9dd6a9d93534456e383c4a97
Nazarko19 <suhorukyinazar@gmail.com>
2021-08-30 11:21:06 +0300

    homework9

--- src/App.js
+++ src/App.js
@@ -2,39 +2,12 @@
 
 export default function App() {
 
-let onSubmitForm = (e) => {
-     e.preventDefault()
-    let name = e.target.name.value
-    let user = {name}
-    addUser(user).then(value => {
-        dispatch({type:'Push_Users',payload:value})
-    })
-
-
-
-    }
-    //
-    // let [form,setForm] = useState({name: 'name'})
-    //
-    //
-    // const onChange = (e) => {
-    //   setForm({...form,[e.target.name]:e.target.value})
-    // }
 
     return (
 
 
        <div>
 
-           <form onSubmit={onSubmitForm}>
-               <input type="text" name={'name'}  />
-               {/*value={form.name} onChange={onChange}*/}
-               <button>addUser</button>
-           </form>
-
-           {
-               users.map(value =><div>{value.name}</div>)
-           }
        </div>
   );
 }
