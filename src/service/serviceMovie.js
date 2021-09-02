bef3d3132db674739a9fa67224a5e089dca5f280
Nazarko19 <suhorukyinazar@gmail.com>
2021-09-02 17:33:13 +0300

    homework9

--- src/service/serviceMovie.js
+++ src/service/serviceMovie.js
@@ -1,4 +1,15 @@
 import axios from 'axios'
 
 
-axios
+let url= axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    headers:{
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMjZlODlmZDQ5NTRjMzIyNzBkZjNhZjdhMmRiOWUyYSIsInN1YiI6IjYxMmY1ZWY5MmNlZmMyMDAyYzEyOGVjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.a4BEefO3f8OV6744igWKhxQsNZjW1C1IvxyzjDciJyw'
+    }
+})
+
+const getMovie = () => {
+  return url.get('/discover/movie')
+}
+
+export {getMovie}
