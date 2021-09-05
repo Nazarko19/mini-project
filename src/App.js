c64ba8861089dba869fe4d4f63c2bafe677b2293
Nazarko19 <suhorukyinazar@gmail.com>
2021-09-05 12:07:06 +0300

    homework9

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Genres from "./components/genres/Genres";
 import MoviesLists from "./components/movieslist/MoviesLists";
-
+import {Link} from "react-router-dom";
 
 export default function App() {
 
