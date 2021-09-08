9a4b3d401a491937c0db4aae99bdef0d287438b0
Nazarko19 <suhorukyinazar@gmail.com>
2021-09-08 14:24:03 +0300

    mini-project

--- src/components/filmDeteils/FilmDeteils.js
+++ src/components/filmDeteils/FilmDeteils.js
@@ -1,10 +1,12 @@
 export default function FilmDeteils(props) {
+  console.log(props)
   let {location:{state}} = props
   return (
     <div>
 
       {
-        state.id && <>{state.title} </>
+        state.id && <>{state.title} {state.original_title}
+          {state.overview} </>
       }
 
     </div>
